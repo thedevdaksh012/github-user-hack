4c143aa62148ca06d8409fdbaad38e23c6226ff3
thedevdaksh012 <115072272+thedevdaksh012@users.noreply.github.com>
2022-10-10 14:42:18 +0530

    added this keyword

---
+++ useStrict.js
@@ -0,0 +1,18 @@
+x = 3.14;    // This will not cause an error.
+myFunction();
+
+function myFunction() {
+  "use strict";
+  y = 3.14;  // This will cause an error (y is not defined).
+}
+
+
+// this keyword
+const person = {
+  firstName: "John",
+  lastName : "Doe",
+  id       : 5566,
+  fullName : function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
